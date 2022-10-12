3509d2a184f1bf2ef14ff3c9453c82292fdb3e9e
ChrLion <christopher@loewenstein.family>
2022-10-12 14:36:34 +0200

    added chess board with class

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import pygame as py
 
-# Define some colors
+
+class Board:
+    def __init__(self):
+        self.size_x = 80
+        self.size_y = 80
+        self.position = 40
+        self.mask = 1
+
+    def draw_board(self):
+        for u in range(4):
+            for i in range(4):
+                py.draw.rect(screen, BEIGE, [self.size_x*2*i+self.position, self.size_y*u*2+self.position, self.size_x, self.size_y])
+                py.draw.rect(screen, BROWN, [self.size_x*2*i+3*self.position, self.size_y*u*2+self.position, self.size_x, self.size_y])
+        for u in range(4):
+            for i in range(4):
+                py.draw.rect(screen, BROWN, [self.size_x*2*i+self.position, self.size_y*u*2+3*self.position, self.size_x, self.size_y])
+                py.draw.rect(screen, BEIGE, [self.size_x*2*i+3*self.position, self.size_y*u*2+3*self.position, self.size_x, self.size_y])
+
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BROWN = (133, 94, 66)
+BEIGE = (255, 233, 197)
 
 py.init()
-
+Board = Board()
 # Set the width and height of the screen [width, height]
 size = (1080, 720)
 screen = py.display.set_mode(size)
@@ -28,8 +48,8 @@ while True:
             quit()
 
     screen.fill(WHITE)
+    Board.draw_board()
 
     py.display.flip()
 
-    # --- Limit to 60 frames per second
     clock.tick(30)
