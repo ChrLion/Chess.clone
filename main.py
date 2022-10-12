d87caa6ced7fc63b84de743d796a95d9643a5d72
ChrLion <christopher@loewenstein.family>
2022-10-12 14:09:31 +0200

    changed syntax: pygame to py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-#Yaman
-print("Test")
 import pygame as py
-
-import pygame
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -10,30 +6,30 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-pygame.init()
+py.init()
 
 # Set the width and height of the screen [width, height]
 size = (1080, 720)
-screen = pygame.display.set_mode(size)
+screen = py.display.set_mode(size)
 
-pygame.display.set_caption("Chess")
+py.display.set_caption("Chess")
 
 # Loop until the user clicks the close button.
 done = False
 
 # Used to manage how fast the screen updates
-clock = pygame.time.Clock()
+clock = py.time.Clock()
 
 # -------- Main Program Loop -----------
 while True:
     # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in py.event.get():
+        if event.type == py.QUIT:
             quit()
 
     screen.fill(WHITE)
 
-    pygame.display.flip()
+    py.display.flip()
 
     # --- Limit to 60 frames per second
     clock.tick(30)
